e8804c913fb0de84eec6da85792d16fdad06c0a9
Ugur G <ugur.guelenoglu@web.de>
2016-09-07 09:17:11 +0200

    commit vaadin project

---
+++ vaadin-archetype-application/src/main/java/com/vaadin/LoginData.java
@@ -0,0 +1,44 @@
+package com.vaadin;
+
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class LoginData {
+
+	
+	@NotNull(message = "Please enter your email")
+	private String email;
+	
+	@NotNull(message = "Please enter a password")
+	@Size(min = 6, max = 32, message = "Your password must be at least 6 characters long.")
+	private String password;
+	
+	@NotNull(message = "Please enter a password")
+	@Size(min = 6, max = 32, message = "Your password must be at least 6 characters long.")
+	private String retype;
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRetype() {
+		return retype;
+	}
+	public void setRetype(String retype) {
+		this.retype = retype;
+	}
+	
+	
+	
+
+}
---
+++ vaadin-archetype-application/src/main/java/com/vaadin/SettingsView.java
@@ -0,0 +1,34 @@
+package com.vaadin;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+public class SettingsView extends VerticalLayout implements View{
+	
+	
+	private TextField email = new TextField("Your email: ");
+	private TextField username = new TextField("Your username: ");
+	
+	private Button save = new Button("Save changes");
+	private Button cancel = new Button("Cnacel changes");
+	private Button recipes = new Button("Your recipes");
+	
+	
+	public SettingsView() {
+		
+		addComponents(save,cancel, recipes);
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
